e21134acf68161aa9b59a81c8f25a40bd4222baf
Elizaveta1094 <115744657+Elizaveta1094@users.noreply.github.com>
2022-11-22 15:55:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-Console.WriteLine("Введите цифру : ");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите цифру недели от 1 до 7 : ");
 int number = Convert.ToInt32(Console.ReadLine());
-int [] array = { number };
-Console.WriteLine( array [2]);
-
-
+if(number == 1)
+{
+    Console.WriteLine("Нет");
+}
+if(number == 2)
+{
+    Console.WriteLine("Нет");
+}
+if(number == 3)
+{
+    Console.WriteLine("Нет");
+}
+if(number == 4)
+{
+    Console.WriteLine("Нет");
+}
+if(number == 5)
+{
+    Console.WriteLine("Нет");
+}
+if(number == 6)
+{
+    Console.WriteLine("Да");
+}
+if(number == 7)
+{
+    Console.WriteLine("Да");
+}
